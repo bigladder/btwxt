404a2d41c4e23c9c4b54cc857b321627a10bb95b
Tanaya Mankad <tanaya.mankad@bigladdersoftware.com>
2023-04-04 13:02:07 -0600

    Add missing namespace designator.

--- include/btwxt/logging.h
+++ include/btwxt/logging.h
@@ -5,28 +5,32 @@
 
 #include <courierr/courierr.h>
 
-class BtwxtLogger : public Courierr::Courierr {
-  public:
-    void error(const std::string_view message) override { write_message("ERROR", message); }
-    void warning(const std::string_view message) override { write_message("WARNING", message); }
-    void info(const std::string_view message) override { write_message("NOTE", message); }
-    void debug(const std::string_view message) override { write_message("DEBUG", message); }
+namespace Btwxt {
 
-  protected:
-    void write_message(const std::string_view message_type, const std::string_view message)
-    {
-        std::string context_string =
-            message_context
-                ? fmt::format(" ({})", *(reinterpret_cast<std::string*>(message_context)))
-                : "";
-        std::cout << fmt::format("  [{}]{} {}", message_type, context_string, message) << std::endl;
-    }
-};
+    class BtwxtLogger : public Courierr::Courierr {
+    public:
+        void error(const std::string_view message) override { write_message("ERROR", message); }
 
-class BtwxtException : public Courierr::CourierrException {
-  public:
-    explicit BtwxtException(const std::string& message, Courierr::Courierr& logger)
-        : CourierrException(message, logger)
-    {
-    }
-};
+        void warning(const std::string_view message) override { write_message("WARNING", message); }
+
+        void info(const std::string_view message) override { write_message("NOTE", message); }
+
+        void debug(const std::string_view message) override { write_message("DEBUG", message); }
+
+    protected:
+        void write_message(const std::string_view message_type, const std::string_view message) {
+            std::string context_string =
+                    message_context
+                    ? fmt::format(" ({})", *(reinterpret_cast<std::string *>(message_context)))
+                    : "";
+            std::cout << fmt::format("  [{}]{} {}", message_type, context_string, message) << std::endl;
+        }
+    };
+
+    class BtwxtException : public Courierr::CourierrException {
+    public:
+        explicit BtwxtException(const std::string &message, Courierr::Courierr &logger)
+                : CourierrException(message, logger) {
+        }
+    };
+}
